0b497d384870424db018ae3a11c8ab2825eeeb89
Andy Fisher <afisher2@users.noreply.github.com>
2022-11-13 12:41:24 -0600

    slide

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CalibrateSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CalibrateSlide.java
@@ -3,24 +3,60 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 @TeleOp
 public class CalibrateSlide extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         DcMotor slideMotor = hardwareMap.get(DcMotor.class, "slideMotor");
+        TouchSensor mag = hardwareMap.get(TouchSensor.class, "magSense");
+        slideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         // Reset the encoder to 0
         slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         // Tells the motor to run until we turn it off
         slideMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         waitForStart();
+
+        boolean bottom = false;
+        boolean top = false;
         while(opModeIsActive()){
 
-            if(gamepad1.a) {
-                slideMotor.setPower(1.0);
-            } else if(gamepad1.b) {
-                slideMotor.setPower(-1.0);
+            if(mag.isPressed()) {
+                if(!bottom) {
+                    bottom = true;
+                    slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    slideMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                }
+            } else {
+                bottom = false;
+            }
+
+            //5361
+            if(slideMotor.getCurrentPosition() <= -3000) {
+                top = true;
+            } else {
+                top = false;
+            }
+
+            if(gamepad1.cross) {
+                if (!top) {
+                    slideMotor.setPower(-0.5);
+                }
+                else
+                {
+                    slideMotor.setPower(0.05);
+                }
+            } else if(gamepad1.circle) {
+                if(!bottom) {
+                    slideMotor.setPower(0.5);
+                }
+                else
+                {
+                    slideMotor.setPower(0.0);
+                }
             } else {
                 slideMotor.setPower(0);
             }
