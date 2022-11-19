67027c284d9ac3ca0ccadbec06a27ed8a4cd49ad
Andy Fisher <afisher2@users.noreply.github.com>
2022-11-18 18:42:53 -0600

    slide

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualDriveDookie.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualDriveDookie.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -16,7 +17,6 @@ public class ManualDriveDookie extends LinearOpMode {
     DcMotor leftFront;
     DcMotor rightBack;
     DcMotor rightFront;
-    HunkOfMetal hunk;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -24,20 +24,27 @@ public class ManualDriveDookie extends LinearOpMode {
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
         rightBack = hardwareMap.get(DcMotor.class, "rightBack");
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
-        hunk = new HunkOfMetal(this);
+        CRServo intakeWheel = hardwareMap.get(CRServo.class, "leftIntake");
+        CRServo intakeWheelDeux = hardwareMap.get(CRServo.class, "rightIntake");
+        DcMotor slideMotor = hardwareMap.get(DcMotor.class, "slideMotor");
+        slideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        TouchSensor mag = hardwareMap.get(TouchSensor.class, "magSense");
+
+        // Reset the encoder to 0
+        slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        // Tells the motor to run until we turn it off
+        slideMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
 
         // Stops coasting
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        hunk.initialize();
 
         waitForStart();
-        boolean encoderReset = false;
-        int level = 0;
-        int turnPosition = 0;
-        long ttableStart = 0;
+        boolean bottom = false;
+        boolean top = false;
         while (opModeIsActive()) {
 
             /*****************************/
@@ -61,27 +68,58 @@ public class ManualDriveDookie extends LinearOpMode {
             rightBack.setPower(rightX - rightY + leftX);
             rightFront.setPower(rightX - rightY - leftX);
 
+
+            // put in code from IntakeTest
             if (gamepad1.a) {
-                hunk.spinWheelsInRight();
-                hunk.spinWheelsInLeft();
+                intakeWheel.setPower(1.0);
+                intakeWheelDeux.setPower(-1.0);
             } else if (gamepad1.b) {
-                hunk.spinWheelsOutRight();
-                hunk.spinWheelsOutLeft();
-            }
-
-            //Wind/unwind linear slide
-            if (gamepad1.x) {
-                hunk.wind();
-
-            } else if (gamepad1.y) {
-                hunk.unwind();
+                intakeWheel.setPower(-1.0);
+                intakeWheelDeux.setPower(1.0);
             } else {
-                hunk.stopWind();
+                intakeWheel.setPower(0.0);
+                intakeWheelDeux.setPower(0.0);
             }
-
-            // Linear slide
-            telemetry.addData("ticks", hunk.getTicks());
-            telemetry.update();
         }
+
+        // put in code from CalibrateSlide
+        if(mag.isPressed()) {
+            if(!bottom) {
+                bottom = true;
+                slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                slideMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            }
+        } else {
+            bottom = false;
+        }
+        //5361
+        if(slideMotor.getCurrentPosition() <= -3000) {
+            top = true;
+        } else {
+            top = false;
+        }
+
+        if(gamepad1.cross) {
+            if (!top) {
+                slideMotor.setPower(-0.5);
+            }
+            else
+            {
+                slideMotor.setPower(0.05);
+            }
+        } else if(gamepad1.circle) {
+            if(!bottom) {
+                slideMotor.setPower(0.5);
+            }
+            else
+            {
+                slideMotor.setPower(0.0);
+            }
+        } else {
+            slideMotor.setPower(0);
+        }
+
+        telemetry.addData("ticks", slideMotor.getCurrentPosition());
+        telemetry.update();
     }
 }
