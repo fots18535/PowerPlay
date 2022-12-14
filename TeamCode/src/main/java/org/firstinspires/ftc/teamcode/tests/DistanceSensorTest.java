8f7ed0c1f51bc35d533ccc49bedc1ff1b6be0875
Andy Fisher <afisher2@users.noreply.github.com>
2022-12-13 20:55:17 -0600

    lazers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/DistanceSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/DistanceSensorTest.java
@@ -11,11 +11,38 @@ public class DistanceSensorTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         DistanceSensor lazerLeft = hardwareMap.get(DistanceSensor.class, "lazerLeft");
         DistanceSensor lazerRight = hardwareMap.get(DistanceSensor.class, "lazerRight");
+
         waitForStart();
+
         while(opModeIsActive()){
-            telemetry.addData("lazerLeft", lazerLeft.getDistance(DistanceUnit.INCH));
-            telemetry.addData("lazerRight", lazerRight.getDistance(DistanceUnit.INCH));
+
+            double a = lazerLeft.getDistance(DistanceUnit.INCH);
+            double b = lazerRight.getDistance(DistanceUnit.INCH);
+            telemetry.addData("lazerLeft", a);
+            telemetry.addData("lazerRight", b);
+
+
+
+           if(b > 5.0 && b < 12.0 && a > 12.0)
+            {
+                telemetry.addData("scenario", "B");
+                telemetry.addData("turn", "right 40");
+                //telemetry.addData("forward", (b - 5) * Math.cos(Math.toRadians(90.0 - 50.0)));
+                //telemetry.addData("slide", (b - 5) * Math.sin(Math.toRadians(90.0 - 50.0)));
+            }
+            else if(a > 5.0 && a < 12.0 && b > 12.0)
+            {
+                telemetry.addData("scenario", "C");
+                telemetry.addData("turn", " left 40");
+                //telemetry.addData("forward", (a - 5) * Math.cos(Math.toRadians(90.0 - 50.0)));
+                //telemetry.addData("slide", (a - 5) * Math.sin(Math.toRadians(90.0 - 50.0)));
+            }else {
+               telemetry.addData("scenario", "?");
+               telemetry.addData("turnm  m ", "?");
+           }
+
             telemetry.update();
+
         }
     }
 }
